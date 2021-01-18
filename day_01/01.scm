a3fe37c377176b3655395360aa8ab19b62f28063
Phil <phil@beadling.co.uk>
2021-01-18 01:26:15 +0000

    comments

--- day_01/01.scm
+++ day_01/01.scm
@@ -1,12 +1,13 @@
-(use-modules (ice-9 textual-ports)
-	     (srfi srfi-1))
+(use-modules (ice-9 textual-ports) ;; get-string-all
+	     (srfi srfi-1)) ;; drop-right
 
 (define entry-list
-  (drop-right
+  (drop-right ;; remove right-most element (from empty string?)
    (map string->number
 	(string-split
-	(call-with-input-file "input.txt" get-string-all)
-	#\newline)) 1))
+	 (call-with-input-file "input.txt" get-string-all)
+	 #\newline))
+   1))
 
 (display entry-list)
 

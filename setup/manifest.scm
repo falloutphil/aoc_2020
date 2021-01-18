48e911c0c9fb458d1f4c191ecf332d4b9691eea5
Phil <phil@beadling.co.uk>
2021-01-18 01:11:28 +0000

    comments

--- setup/manifest.scm
+++ setup/manifest.scm
@@ -1,3 +1,5 @@
+;; Install a guix profile for guile dev environment
+;; guix package -m manifest.scm -p ~/guix/profiles/aoc
 (specifications->manifest
  '("emacs" "emacs-guix" "emacs-geiser" "emacs-paredit" "emacs-magit" "emacs-zenburn-theme" "emacs-smex" "emacs-flx" "emacs-key-chord" "emacs-which-key" "emacs-flycheck-guile"
    "git" "nss-certs" "guile"))
